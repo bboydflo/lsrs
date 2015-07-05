8ed56c8307167898b24fd921e388c8cba834f61c
Florin Onciu <bboydflo@gmail.com>
2015-07-06 00:22:58 +0200

    added exploreaza.js

---
+++ public/js/exploreaza.js
@@ -0,0 +1,15 @@
+$(function() {
+	// on document ready
+	
+	$( '.exploreaza' ).on( 'click', function( e ) {
+		// prevent default event
+		e.preventDefault();
+
+		// get current city
+		var city = $( e.target ).parents( '.item-content' ).find( 'h5' ).text().toLowerCase();
+
+		// log city
+		console.log( city );
+	}); 
+
+});
