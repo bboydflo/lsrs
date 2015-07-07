0e7d754268d0741211a87d73ae5d7b78ee5ccc12
Florin Onciu <bboydflo@gmail.com>
2015-07-08 00:23:33 +0200

    added hbs instead of express-handlebars

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 var express = require('express');
-var exphbs  = require('express-handlebars');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var app = express();
+
+// require handlebars
+require('hbs');
 
 // set default views directory
 app.set('views', path.join(__dirname, 'views'));
 
 // set express view engine
-app.engine('.hbs', exphbs({extname: '.hbs', defaultLayout: 'main'}));
-app.set('view engine', '.hbs');
+app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public/images/icons/', 'favicon.ico')));
