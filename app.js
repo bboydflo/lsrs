bd8342aa27bb636b8fef269cc0abceea2bea8261
Florin Onciu <bboydflo@gmail.com>
2015-07-08 02:14:48 +0200

    switched to other  express handlebars implementation

--- app.js
+++ app.js
@@ -6,14 +6,10 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var app = express();
 
-// require handlebars
-require('hbs');
-
-// set default views directory
-app.set('views', path.join(__dirname, 'views'));
-
 // set express view engine
 app.set('view engine', 'hbs');
+// set default views directory
+app.set('views', path.join(__dirname, 'views'));
 
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public/images/icons/', 'favicon.ico')));
@@ -22,13 +18,16 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static('public'));
 
-// get an instance of router
-var router = express.Router();
+// static files
+app.use('/assets', express.static(__dirname + '/public/assets'));
+app.use('/css', express.static(__dirname + '/public/css'));
+app.use('/images', express.static(__dirname + '/public/images'));
+app.use('/js', express.static(__dirname + '/public/js'));
+app.use('/pages-js', express.static(__dirname + '/public/js/pages-js'));
 
 // website route
-router.get('/', function(req, res) {
+app.get('/', function(req, res) {
     // log
     console.log('index');
     // send static page
@@ -38,24 +37,43 @@ router.get('/', function(req, res) {
 });
 
 // login route
-router.get('/login', function(req, res) {
+app.get('/login', function(req, res) {
     // render login page
-    res.render('login', {});
+    res.render('login', {'path': 'login'});
 });
 
 // register route
-router.get('/register', function(req, res) {
+app.get('/register', function(req, res) {
     // render login page
-    res.render('main', {'register': true});
+    res.render('register', {'path': 'register', 'register': true});
 });
 
+var routeExploreaza = express.Router();
+
 // exploreaza route
-router.get('/exploreaza', function(req, res) {
-    res.render('exploreaza', {'acasa': false, 'page-title': 'Exploreaza', 'path': 'exploreaza'});
+routeExploreaza.get('/', function(req, res) {
+    // get query filter
+    var filter = req.query.filter;
+
+    if(typeof filter === 'string' && filter.length) {
+        res.render('locatii', {
+            'acasa': false,
+            'page-title': 'Exploreaza',
+            'path': 'locatii',
+            'filter': filter
+        });
+    } else {
+        res.render('exploreaza', {
+            'acasa': false,
+            'page-title': 'Exploreaza',
+            'path': 'exploreaza',
+            'src': '../public'
+        });
+    }
 });
 
 // exploreaza route with parameter
-router.get('/exploreaza/:filter', function(req, res) {
+/*routeExploreaza.get('/:filter', function(req, res) {
     // local vars
     var all = false, filter = req.params.filter;
     // check filter
@@ -63,17 +81,22 @@ router.get('/exploreaza/:filter', function(req, res) {
         // all -> true
         all = true;
     }
-    res.render('locatii', {'acasa': false, 'page-title': 'Exploreaza', 'path': 'locatii', 'all': all, 'filter': filter});
+    res.render('locatii', {
+        'acasa': false,
+        'page-title': 'Exploreaza',
+        'path': 'locatii',
+        'all': all,
+        'filter': filter
+    });
     // res.send('hello ' + req.params.filter + '!');
-});
+});*/
 
 app.get('/full', function(req, res) {
     // res.sendfile(__dirname + '/public/full-page.html');
     res.sendfile('./public/full-page.html');
 });
 
-// apply the routes to our application
-app.use('/', router);
+app.use('/exploreaza', routeExploreaza);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
